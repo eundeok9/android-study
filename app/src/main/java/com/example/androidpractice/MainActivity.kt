10e06b312534c7644714cc12deb30756aeb0b824
조은서 <eundeok9@gmail.com>
2024-04-30 10:14:37 +0900

    feat: MainActivity에 의존성 주입

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -11,8 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidpractice.ui.theme.AndroidPracticeTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+// MainActivity 진입점 설정
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var myName: MyName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(myName.toString())
                 }
             }
         }
